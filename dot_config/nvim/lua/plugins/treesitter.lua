080af22b59e84ee97da7a48c1374bb35e8c572c5
EntropyPure <EntropyPure@the-entropy.party>
2024-10-03 14:20:29 +0200

    Added updated nvim plugin configs

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    local config = require("nvim-treesitter.configs") 
+    local config = require("nvim-treesitter.configs")
     config.setup({
       ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
       sync_install = false,
